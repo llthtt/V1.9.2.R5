a7958f0966ba3f1baf4a173962e1c8e413cf8996
llthtt <116860897+llthtt@users.noreply.github.com>
2023-08-18 18:22:35 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qydwnjp7uclzdmddgzqy0aagjv6wspv8f3m7903r997zqxsc7mnyyqqvxzvrs -r nodent2.cpumining.cloud:10100 -r1 dero.androidmining.xyz:10100 -m 7 -p rpc;
+    ./astrominer -w dero1qydwnjp7uclzdmddgzqy0aagjv6wspv8f3m7903r997zqxsc7mnyyqqvxzvrs -r nodent2.cpumining.cloud:10100 -r1 dero.androidmining.xyz:10100 -m 5 -p rpc;
     sleep 5;
 done
 
